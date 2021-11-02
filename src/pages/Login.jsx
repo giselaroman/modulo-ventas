da4c4a0d056043b4dd57159c247bff78292909a7
gisela roman <romanjaimes1211@gmail.com>
2021-11-02 11:51:32 +1000

    edicion de la pagina de login para crear formulario

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
         <div>
-            pagina para el login
+            <h2>Inicia sesion en tu cuenta</h2>
+            <form>
+                <div>
+                    <input type='email' placeholder='dsl@c.com' required/>
+                    <input type='password' required/>
+                </div>
+                <div>
+                    <label htmlFor="Recuerdame">
+                        <input type='checkbox' name='Recuerdame'/> 
+                        Recuerdame
+                    </label> 
+                </div>
+                <div>
+                    <Link to='/'>
+                        Olvidaste tu contrase'a?
+                    </Link>
+                </div>
+                <div>
+                    <Link htmlFor='/admin'>
+                    <button type='submit'>
+                        Iniciar Sesion
+                    </button>
+                    </Link>
 
+                </div>
+            </form>    
         </div>
     )
 }
 
-export default Login
+export default Login;
